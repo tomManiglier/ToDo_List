cdae902908fb2825eca090c66ed2339396bbce3a
Tom Maniglier <tom.maniglier@gmail.com>
2022-11-16 23:27:15 +0100

    v5 Local Storage & Debug

--- js/script.js
+++ js/script.js
@@ -4,12 +4,27 @@ const list = document.querySelector('#list');
 const form = document.querySelector('#form');
 const empty = document.querySelector('#empty');
 
+list.innerHTML = localStorage.getItem('localList')
+
+const spanDeletes = document.querySelectorAll('.delete');
+
+for (let span of spanDeletes) {
+    span.onclick = () => del(span.parentElement);
+}
+
+if (list.innerHTML == '') {
+    empty.style.display = 'block';
+} else {
+    empty.style.display = 'none';
+}
+
 form.addEventListener('submit', (e) => {
     const li = document.createElement('li');
     const span = document.createElement('span');
 
     empty.style.display = 'none';
 
+    span.classList.add('delete');
     span.innerHTML = '<img src="img/delete.png" alt="Delete Icon">';
     li.innerHTML = input.value;
 
@@ -18,8 +33,11 @@ form.addEventListener('submit', (e) => {
     li.appendChild(span);
     list.appendChild(li);
 
+    
     input.value = '';
     e.preventDefault();
+
+    localStorage.setItem('localList', list.innerHTML);
 });
 
 function del(element) {
@@ -27,5 +45,9 @@ function del(element) {
 
     if (list.innerHTML == '') {
         empty.style.display = 'block';
+    } else {
+        empty.style.display = 'none';
     }
+
+    localStorage.setItem('localList', list.innerHTML);
 }
