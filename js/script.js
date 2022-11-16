909f09ededf6f1d6edba7c730f726e9cbefd604c
Tom Maniglier <tom.maniglier@gmail.com>
2022-11-16 22:52:21 +0100

    v4 Empty fonctionnality

--- js/script.js
+++ js/script.js
@@ -2,10 +2,13 @@ const input = document.querySelector('#input');
 const submit = document.querySelector('#submit');
 const list = document.querySelector('#list');
 const form = document.querySelector('#form');
+const empty = document.querySelector('#empty');
 
 form.addEventListener('submit', (e) => {
     const li = document.createElement('li');
     const span = document.createElement('span');
+
+    empty.style.display = 'none';
 
     span.innerHTML = '<img src="img/delete.png" alt="Delete Icon">';
     li.innerHTML = input.value;
@@ -21,4 +24,8 @@ form.addEventListener('submit', (e) => {
 
 function del(element) {
     element.remove();
+
+    if (list.innerHTML == '') {
+        empty.style.display = 'block';
+    }
 }
